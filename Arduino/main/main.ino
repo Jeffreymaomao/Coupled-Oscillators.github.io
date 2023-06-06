a9d05e99d0e56d6cd6080c39857d9060934678d6
Chang Mao Yang <jeffrey0613mao@gmail.com>
2023-06-06 15:13:59 +0800

    arduino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -4,28 +4,35 @@
 
 VL53L0X sensor;
 
+double distance0 = 0.0;
+
+int const btn = 8;
+
 void setup() {
     Serial.begin(9600);
+    pinMode(btn, INPUT);
     Wire.begin();
     sensor.setAddress(0x30);
     delay(1000);
     if (!sensor.init()) {
-        while(1){
-            Serial.println("none");
-        }
+        Serial.println("none");
     }
     sensor.setTimeout(500);
-    sensor.setMeasurementTimingBudget(200000);
 }
 
 void loop() {
-    double distance = sensor.readRangeSingleMillimeters();
+    double distance = sensor.readRangeSingleMillimeters()/10.0; // unit (cm)
+
+    bool press = digitalRead(btn);
+
+    if(press){distance0 = distance;}
     if (sensor.timeoutOccurred()) {
         Serial.println("Reading distance failed!");
     } else {
         Serial.print("Distance: ");
-        Serial.print(distance/10);
+        Serial.print(distance-distance0);
         Serial.println(" cm");
     }
+    
 
 }
